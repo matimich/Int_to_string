d441540d41d7dcb726da25175c52a95da1a998fe
matimich <matimich@vp.pl>
2018-09-11 00:24:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,9 @@
 
 int main(void)
 {
+    //EXAMPLE
     int a =-100;
-    unsigned int b =105;
+    unsigned int b = 105;
 
     printf("\n%s\n",IntToString(&a));   //display result
     printf("\n%s\n",UIntToString(&b));   //display result
